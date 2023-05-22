90c2fdb4d7f4eddc8de935f0447edb519c38bb37
Hans Hüppelshäuser <Hans.Hueppelshaeuser@ibm.com>
2023-05-22 10:06:15 +0200

    Adding Hello World

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.post('/findRide', (req, res) => {
   res.json({ rideCost });
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
